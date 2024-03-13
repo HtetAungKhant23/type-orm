1f9692686c89b7b63548836f7d9b3ee872f6bd44
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-03-13 16:36:34 +0630

    feat(module): import TypeOrm module in app module and add env config and add .env, .env example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envConfig } from './config/env.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: envConfig.host,
+      port: envConfig.port,
+      username: envConfig.username,
+      password: envConfig.password,
+      database: envConfig.database,
+      entities: ['src/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
