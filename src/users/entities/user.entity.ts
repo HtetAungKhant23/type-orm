13215d8800f19dd441d521c98df863fb270bc943
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-03-13 16:54:14 +0630

    feat(entity): add user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// ? Entity is a class that maps to a database table (or collection when using MongoDB).
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  first_name: string;
+
+  @Column('text')
+  last_name: string;
+
+  @Column('text')
+  email: string;
+}
